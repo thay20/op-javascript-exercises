d607f4129074041cc869dd1a95718bb14d309758
thay20 <thay20@proton.me>
2022-11-01 23:05:13 -0400

    Complete exercises 1 thru 3 (including 3)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+  let myString = '';
+  if(num >= 0){
+    for(let i = 0; i < num; i++){
+    myString += word;  
+    }
+    return myString;
+  } else {
+    return 'ERROR';
+  } 
 };
 
 // Do not edit below this line
