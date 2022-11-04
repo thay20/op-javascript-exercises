a5eed9d1969463eddd46038f27c587ca1adde4ba
thay20 <thay20@proton.me>
2022-11-03 23:47:30 -0400

    Create function to remove items from array
    Needed to remove an unknown quantity of items from a given array.
    Decided to loop through the array based on the indexOf the item and
    remove each instance that was found. Added index.html to load the script
    and log each step for verification.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
 const removeFromArray = function() {
-  
+  const myArray = arguments[0];
+  let args = Array.from(arguments);
+  args.shift();
+
+  for(const arg of args) {
+    if(myArray.indexOf(arg) < 0) {
+      continue;
+    } else {
+      while(myArray.indexOf(arg) >= 0){
+        myArray.splice(myArray.indexOf(arg), 1)
+      }
+    }
+  }
+  return myArray;
 };
+
+//run the function for stepper
+//console.log(removeFromArray([1,2,3,4],3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
