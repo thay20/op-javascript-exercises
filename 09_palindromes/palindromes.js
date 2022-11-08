fc82a2ad95e3972dc89281b01b8d106994d6be65
thay20 <thay20@proton.me>
2022-11-07 22:24:45 -0500

    Set up palindrome verification

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string = string.replace(/\W/g, '');
+  let rString = '';
+  for(let i = string.length-1; i >= 0; i--) {
+    rString += string[i];
+  }
+  
+  if(rString.toLowerCase() === string.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
