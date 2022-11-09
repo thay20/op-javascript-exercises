40303c2d40047c0de023e099455c5166cee5a8f4
thay20 <thay20@proton.me>
2022-11-08 20:56:04 -0500

    Create findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const date = new Date();
+  const age = [];
+  let oldest;
+  
+  for(let i = 0; i < people.length; i++) {
+    let birth = people[i].yearOfBirth;
+    let death = people[i].yearOfDeath || date.getFullYear();
+    age[i] = death - birth;
+  }
+  oldest = age.indexOf(Math.max(...age));
+  return people[oldest];
 };
 
 // Do not edit below this line
