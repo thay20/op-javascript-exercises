447318f36ad37ca020f1bdc3638e40c5763b8da4
thay20 <thay20@proton.me>
2022-11-10 21:40:27 -0500

    Create getUnicode function to convert string
    Created function to convert string to unicode. Beginnings of shifting
    the code for cipher in shiftUnicode. Currently adds the shift to the
    unicode per given character.

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,4 +1,30 @@
-const caesar = function() {
+const caesar = function(oString, shift) {
+
+  function getUnicode(string) {
+    let unicodeArray = [];
+
+    for(let i = 0; i < string.length; i++){
+      unicodeArray.push(string.charCodeAt(i));
+    }
+    return unicodeArray;
+  }
+
+  function shiftUnicode(unicodeArray, shift) {
+    let shiftedArray = [];
+    
+//add an if(maybe switch?) to only shift alpha unicode range, and loop if necessary
+    for(let i = 0; i < unicodeArray.length; i++){
+      shiftedArray[i] = parseFloat(unicodeArray) + parseFloat(shift);
+    }
+    return shiftedArray;
+  }
+
+  function getShiftedString(shiftedArray) {
+
+  }
+
+//testing output before entire caeser is complete
+  console.log(shiftUnicode(getUnicode(oString), shift))
 
 };
 
