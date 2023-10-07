48739d72336c56fd0b217da16df83fea8abac319
thay20 <thay20@proton.me>
2023-10-07 17:13:57 -0400

    Write sumAll function to sum all numbers
    sumAll will take two arguments and return the sum of all integers
    between and including the arguments.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let start = num1;
+  let end = num2;
+  let sum = 0;
+  
+  if (start > end) {
+    start = num2;
+    end = num1;
+  }
 
+  if (typeof num1 != 'number' || typeof num2 != 'number') return 'ERROR';
+  if (num1 == NaN || num2 == NaN) return 'ERROR';
+  if (num1 < 0 || num2 < 0) return 'ERROR';
+ 
+    for (let i = start; i <= end; i++){
+      sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
