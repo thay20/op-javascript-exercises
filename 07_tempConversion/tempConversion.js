97a00de4d41b9cf436cb739d6c1027c18cc800dd
thay20 <thay20@proton.me>
2023-10-07 18:49:34 -0400

    Create functions to convert between degrees C & F

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degF) {
+  let degC;
+  degC = Math.round(((degF - 32) / 1.8) * 10) / 10;
+  return degC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degC) {
+let degF;
+degF = Math.round(((degC*1.8) + 32)*10) / 10;
+return degF;
 };
 
 // Do not edit below this line
