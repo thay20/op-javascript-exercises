df79dba2adfe83bef1fb76fa48692c8293ef1911
thay20 <thay20@proton.me>
2023-10-07 15:53:30 -0400

    Write removeFromArray function
    removeFromArray will take an array and any number of extra arguments
    and create a newArray without any of the unwanted arguments.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  let newArray =[];
+  for (let item of array) {
+    for (let i = 0; i < args.length; i++) {
+      if (item === args[i]) {
+        item = '';
+        break;
+      }
+    }
+    if (item) newArray.push(item);
+  }
+  return newArray;
 };
 
 // Do not edit below this line
