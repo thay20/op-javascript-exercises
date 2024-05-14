9970df74a51a24f149494fbf488a041ae1da053e
thay20 <thay20@proton.me>
2024-05-13 22:29:07 -0400

    Complete lessons 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  
+  function getAge(person) {
+    let age;
+    if (!person.yearOfDeath) {
+      age = new Date().getFullYear() - person.yearOfBirth;
+      } else {
+      age = person.yearOfDeath - person.yearOfBirth;
+      }
+    
+    return age;  
+    }
+  
+  const oldest = people.sort((a,b) => getAge(a) > getAge(b) ? -1 : 1);
+  
+  return oldest[0];
 
 };
 
